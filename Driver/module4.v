021a576707730bb5cc726f0333052fa2e0ede977
A-Hares <139650137+A-Hares@users.noreply.github.com>
2023-07-17 01:55:57 +0300

    Update module4.v

--- Driver/module4.v
+++ Driver/module4.v
@@ -8,20 +8,3 @@ module Driver #(parameter width = 8) (
     assign data_out = data_en ? data_in : 'bz;
 
 endmodule
-
-module DriverTB;
-    localparam width = 8;
-    reg [width-1:0] data_in;
-    reg data_en;
-    wire [width-1:0] data_out;
-
-    Driver #(.width(width)) inst1(.data_in(data_in) , .data_en(data_en), .data_out(data_out));
-
-    initial begin
-        data_in = 123; data_en = 0; #10; 
-        data_in = 123; data_en = 1; #10;
-        data_in = 321; data_en = 0; #10;
-        data_in = 132; data_en = 1; #10;
-    end
-
-endmodule
