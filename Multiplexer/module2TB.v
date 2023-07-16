b6a7992303c7b96e855923cddbec22e5732475b9
A-Hares <139650137+A-Hares@users.noreply.github.com>
2023-07-17 02:02:39 +0300

    Create module2TB.v

---
+++ Multiplexer/module2TB.v
@@ -0,0 +1,59 @@
+module Mux21
+#(parameter width = 5) 
+(
+    input [width-1:0] in0, in1, 
+    input sel,
+    output [width-1:0] mux_out
+);
+    
+    assign mux_out = sel ? in1 : in0;
+endmodule
+
+module multiplexor_test;
+
+  localparam WIDTH=5;
+
+  reg              sel  ;
+  reg  [WIDTH-1:0] in0  ;
+  reg  [WIDTH-1:0] in1  ;
+  wire [WIDTH-1:0] mux_out;
+
+  Mux21
+  #(
+    .width ( WIDTH )
+   )
+  multiplexor_inst
+  (
+    .sel     ( sel     ),
+    .in0     ( in0     ),
+    .in1     ( in1     ),
+    .mux_out ( mux_out ) 
+  );
+
+  task expect;
+    input [WIDTH-1:0] exp_out;
+    if (mux_out !== exp_out) begin
+      $display("TEST FAILED");
+      $display("At time %0d sel=%b in0=%b in1=%b mux_out=%b",
+               $time, sel, in0, in1, mux_out);
+      $display("mux_out should be %b", exp_out);
+      $finish;
+    end
+   else begin
+      $display("At time %0d sel=%b in0=%b in1=%b, mux_out=%b",
+               $time, sel, in0, in1, mux_out);
+   end 
+  endtask
+
+  initial begin
+    $dumpfile("trytest.vcd");
+    $dumpvars;
+    sel=0; in0=5'h15; in1=5'h00; #1 expect (5'h15);
+    sel=0; in0=5'h0A; in1=5'h00; #1 expect (5'h0A);
+    sel=1; in0=5'h00; in1=5'h15; #1 expect (5'h15);
+    sel=1; in0=5'h00; in1=5'h0A; #1 expect (5'h0A);
+    $display("TEST PASSED");
+    $finish;
+  end
+
+endmodule
